fc65632f324809a135751a7043c2e78a1859444f
theMorshed <themorshedctg@gmail.com>
2023-08-20 22:54:47 +0600

    add tailwind css class for register page styling

--- accounts/forms.py
+++ accounts/forms.py
@@ -46,4 +46,17 @@ class UserRegistrationForm(UserCreationForm):
         )
         
         return our_user
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': (
+                    'appearance-none block w-full bg-gray-200 '
+                    'text-gray-700 border border-gray-200 rounded '
+                    'py-3 px-4 leading-tight focus:outline-none '
+                    'focus:bg-white focus:border-gray-500'
+                )
+            })
             
